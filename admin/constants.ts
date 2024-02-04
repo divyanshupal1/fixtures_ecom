c9359069252b26501545084ff5f85bc67982941e
divyanshupal1 <divyanshupal77@gmail.com>
2024-02-04 12:18:35 +0530

    preview mode:demo

--- admin/constants.ts
+++ admin/constants.ts
@@ -1,8 +1,8 @@
-export const MODE: "demo" | "prod" = "prod"
+export const MODE: "demo" | "prod" = "demo"
 
 function getHost() {
-    if(MODE=='prod') "http://195.35.6.155:8400/" 
-    else "http://localhost:8080/"
+    if(MODE=='prod') return "http://195.35.6.155:8400/" 
+    else return "http://localhost:8080/"
 }
 
 export const LOGO = "Fixtures_ecom"
