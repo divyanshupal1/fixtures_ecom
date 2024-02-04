5058a9009e6940aa48b663d2b5dd9f45bfbc3087
divyanshupal1 <divyanshupal77@gmail.com>
2024-02-04 12:10:51 +0530

    preview fix 3

--- admin/constants.ts
+++ admin/constants.ts
@@ -1,7 +1,12 @@
 export const MODE: "demo" | "prod" = "prod"
 
+function getHost() {
+    if(MODE=='prod') "http://195.35.6.155:8400/" 
+    else "http://localhost:8080/"
+}
+
 export const LOGO = "Fixtures_ecom"
-export const HOST = MODE === 'demo' ? "http://localhost:8080/" : "http://195.35.6.155:8400/"
+export const HOST =  getHost()
 export const BASE_API = HOST + "api/v1"
 export const USER_ROUTE = BASE_API + "/users"
 export const LOGIN_URL = BASE_API + "/users/login"
