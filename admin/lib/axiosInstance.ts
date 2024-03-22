454dae1c37293887712223fedec467a16df920d2
divyanshupal1 <divyanshupal77@gmail.com>
2024-03-23 03:48:38 +0530

    preview fix #4

--- admin/lib/axiosInstance.ts
+++ admin/lib/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { MODE } from "@/constants";
 
 const axiosInstance = axios.create({
-  baseURL: MODE == 'local' ? "http://localhost:8080/api/v1/" : MODE == 'demo' ? "http://localhost:3000/api/v1/" : "http://195.35.6.155:8400/api/v1/",
+  baseURL: MODE == 'local' ? "http://localhost:8080/api/v1/" : MODE == 'demo' ? "http://localhost:3000/api/v1/" : "http://ecom.mymedicos.in/api/v1/",
   withCredentials: true,
 });
 
