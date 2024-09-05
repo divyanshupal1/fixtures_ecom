c7b88cac9331d10a3a78af910258c5aa27dad97a
Divyanshu Pal <divyanshu@srv456226.hstgr.cloud>
2024-09-05 17:03:12 +0000

    fix:backend url fix

--- admin/lib/axiosInstance.ts
+++ admin/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { MODE } from "@/constants";
 
 const axiosInstance = axios.create({
-  baseURL: MODE == 'local' ? "http://localhost:8080/api/v1/" : MODE == 'demo' ? "http://localhost:3000/api/v1/" : "https://admin.acquaso.com/api/v1/",
+  baseURL: MODE == 'local' ? "http://localhost:8080/api/v1/" : MODE == 'demo' ? "http://localhost:3000/api/v1/" : "https://www.acquaso.com/api/v1/",
   withCredentials: true,
 });
 
