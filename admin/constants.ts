8a38704a5903bc436542ab5d9089359ad118b799
divyanshupal1 <divyanshupal77@gmail.com>
2024-11-03 06:31:31 +0530

    constants fix

--- admin/constants.ts
+++ admin/constants.ts
@@ -1,3 +1,4 @@
 export const MODE: "demo" | "prod" | "local" = "prod"
 export const LOGO = "Acquaso"
-export const imgUrl = MODE ==="local" ? "http://localhost:8080/" : "https://www.acquaso.com/"
+// export const imgUrl = "http://localhost:8080/"
+export const imgUrl = "https://www.acquaso.com/"
