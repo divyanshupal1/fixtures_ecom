2cfafdae346b196f7bd88371b3789a3a72ff831f
Devansh Saxena <devanshsaxean1019@gmail.com>
2025-01-09 17:09:52 +0530

    New

--- new-ecom/src/Components/Shared/Posters/MediumPoster.jsx
+++ new-ecom/src/Components/Shared/Posters/MediumPoster.jsx
@@ -1,19 +1,18 @@
 import { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
 import s from "./MediumPoster.module.scss";
 
-const MediumPoster = ({ title, description, posterUrl,textColor,url }) => {
+const MediumPoster = ({ title, description, posterUrl, textColor, url }) => {
   const midPosterRef = useRef();
 
   useEffect(() => {
     midPosterRef.current.style.backgroundImage = `url(${posterUrl})`;
-  });
+  }, [posterUrl]);
 
   return (
     <div className={s.midPoster} ref={midPosterRef}>
       <div className={s.content}>
-        <b style={{color:textColor}}>{title}</b>
-        <p style={{color:textColor}}>{description}</p>
+        <b style={{ color: textColor }}>{title}</b>
+        <p style={{ color: textColor }}>{description}</p>
         <a href={url} className={s.shopNow}>
           Shop Now
         </a>
@@ -21,4 +20,5 @@ const MediumPoster = ({ title, description, posterUrl,textColor,url }) => {
     </div>
   );
 };
+
 export default MediumPoster;
