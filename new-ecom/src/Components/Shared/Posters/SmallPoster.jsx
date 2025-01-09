2d7e2822965687a86192a1b4f9c0230d4fdd8491
Devansh Saxena <devanshsaxean1019@gmail.com>
2025-01-09 17:04:24 +0530

    New

--- new-ecom/src/Components/Shared/Posters/SmallPoster.jsx
+++ new-ecom/src/Components/Shared/Posters/SmallPoster.jsx
@@ -1,18 +1,17 @@
-import { Link } from "react-router-dom";
 import s from "./SmallPoster.module.scss";
 
-const SmallPoster = ({ title, description, posterUrl,url,textColor }) => {
+const SmallPoster = ({ title, description, posterUrl, url, textColor }) => {
   return (
-    <div className={s.smallPoster}>
-      <img src={posterUrl} style={{width:"100%",height:"100%"}} alt="product's poster" />
+    <div className={s.smallPoster} style={{ backgroundImage: `url(${posterUrl})` }}>
       <div className={s.content}>
-        <b style={{color:textColor}}>{title}</b>
-        <p style={{color:textColor}}>{description}</p>
-        <a href="/products" className={s.shopNow}>
+        <b style={{ color: textColor }}>{title}</b>
+        <p style={{ color: textColor }}>{description}</p>
+        <a href={url} className={s.shopNow}>
           Shop Now
         </a>
       </div>
     </div>
   );
 };
+
 export default SmallPoster;
